5d94c8b290722df89e3f0963837e07e332ca1a6b
burntato <kineticz300@gmail.com>
2023-05-28 11:49:31 +0700

    lcd update

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <RTClib.h>
 #include <Servo.h>
 #include <Ultrasonic.h>
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
 
 #define TRIGGER_PIN 12
 #define ECHO_PIN 13
@@ -18,23 +20,31 @@ Servo servo1;
 Servo servo2;
 RTC_DS3231 rtc;
 Ultrasonic ultrasonic1(TRIGGER_PIN, ECHO_PIN);
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   Wire.begin();
   rtc.begin();
   servo1.attach(SERVO_1_PIN);
   servo2.attach(SERVO_2_PIN);
   pinMode(WATER_LEVEL_PIN, INPUT);
+
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Current Time:");
 }
 
-void loop() {
+void loop()
+{
   long depth = ultrasonic1.read();
 
   // RTC
   DateTime now = rtc.now();
 
-  if (depth >= 30 && (now.hour() == 7 || now.hour() == 11 || now.hour() == 17)) {
+  if (depth >= 30 && (now.hour() == 7 || now.hour() == 11 || now.hour() == 17))
+  {
     // 3s
     servo1.write(SERVO_1_OPEN_ANGLE);
     delay(3000);
@@ -44,12 +54,37 @@ void loop() {
   // water level
   int waterLevel = analogRead(WATER_LEVEL_PIN);
 
-  if (waterLevel == 100) {
+  if (waterLevel == 100)
+  {
     // 5s
     servo2.write(SERVO_2_FEED_ANGLE);
     delay(5000);
     servo2.write(SERVO_2_RETURN_ANGLE);
   }
 
-  delay(1000); 
+  lcd.setCursor(0, 1);
+  lcd.print(now.timestamp(DateTime::TIMESTAMP_TIME));
+
+  lcd.setCursor(0, 2);
+  lcd.print("Next feeding time is");
+
+  if (now.hour() < 7)
+  {
+    lcd.print(" 7:00 AM");
+  }
+  else if (now.hour() < 11)
+  {
+    lcd.print(" 11:00 AM");
+  }
+  else if (now.hour() < 17)
+  {
+    lcd.print(" 5:00 PM");
+  }
+  else
+  {
+    lcd.print(" 7:00 AM");
+    lcd.print(now.day() + 1);
+  }
+
+  delay(1000);
 }
